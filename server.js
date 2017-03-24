04e2c3d79b2f99fab0e846ca65910dc6470f7277
Giovany <GiovanyRReyes@gmail.com>
2017-03-24 15:40:11 -0700

    server.js commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+
+
+
+// set our port
+const port = process.env.PORT || 1234
+
+// start app ===================================
+app.listen(port, () => {
+    console.log(`Magic happens on port ${port}`) // notice backwards slashes
+})
+
+app.use(function (req, res) {
+    res.send('hello from node mon')
+})
